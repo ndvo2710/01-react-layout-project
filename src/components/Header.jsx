609652b425135b14f272bcc0102760b592de31e3
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2021-03-08 15:06:01 -0800

    Update project layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 export default class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="https://www.notion.so/ndvo/01-React-Foundation-Projects-fbfd5810eb194af9b0c0202d78a13878">
                         Start Bootstrap
                     </a>
                     <button
@@ -25,23 +25,23 @@ export default class Header extends Component {
                     >
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://www.notion.so/ndvo/01-React-Foundation-Projects-fbfd5810eb194af9b0c0202d78a13878">
                                     Home
                                     <span className="sr-only">(current)</span>
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://www.notion.so/ndvo/01-React-Foundation-Projects-fbfd5810eb194af9b0c0202d78a13878">
                                     About
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://www.notion.so/ndvo/01-React-Foundation-Projects-fbfd5810eb194af9b0c0202d78a13878">
                                     Services
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://www.notion.so/ndvo/01-React-Foundation-Projects-fbfd5810eb194af9b0c0202d78a13878">
                                     Contact
                                 </a>
                             </li>
